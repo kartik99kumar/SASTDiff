3c9d7219f313ce6d8772a62a3361ddaa9edb9dbf
Kartik Kumar <kartik99kumar@gmail.com>
2021-07-06 17:20:06 +0530

    improved log in decl

--- process/decl.py
+++ process/decl.py
@@ -8,18 +8,7 @@ def evaluateDecl(decl, state):
     name = decl.name
     var = Variable(name)
     state.addVariable(var)
-
-    if isinstance(decl.type.type, ast.Struct):
-
-        structName = decl.type.type.name
-        state.addToLog(
-            "variable \"{}\" of structure \"{}\" declared".format(name, structName))
-
-    else:
-
-        state.addToLog("variable \"{}\" declared".format(name))
-
-    init = None
+    state.addToLog("variable \"{}\" added to state".format(var.name))
 
     if isinstance(decl.init, ast.ID):
 
@@ -30,29 +19,31 @@ def evaluateDecl(decl, state):
 
             value = initVar.getValue()
             var.setValue(value)
-            state.addToLog("variable \"{}\" is initialised to variable \"{}\" with value \"{}\"".format(
-                name, initVarName, value))
+            state.addToLog(
+                "variable \"{}\" set to value {}".format(var.name, value))
 
             var.addDependency(initVar)
             state.addToLog(
-                "variable \"{}\" added as dependency of \"{}\"".format(initVarName, name))
+                "variable \"{}\" added as dependency of \"{}\"".format(initVar.name, var.name))
 
     elif isinstance(decl.init, ast.StructRef):
 
         field = process.structref.evaluateStructRef(decl.init, state)
+
         value = field.getValue()
         var.setValue(value)
-        state.addToLog("variable \"{}\" is initialised to field \"{}\" with value {}".format(
-            name, field.name, value))
+        state.addToLog(
+            "variable \"{}\" set to value {}".format(var.name, value))
+
         var.addDependency(field)
         state.addToLog(
-            "variable \"{}\" added as dependency of \"{}\"".format(field.name, name))
+            "variable \"{}\" added as dependency of \"{}\"".format(field.name, var.name))
 
     elif(isinstance(decl.init, ast.Constant)):
 
         value = decl.init.value
         var.setValue(value)
         state.addToLog(
-            "variable \"{}\" is initialised to value \"{}\"".format(name, value))
+            "variable \"{}\" set to value {}".format(var.name, value))
 
     return var
